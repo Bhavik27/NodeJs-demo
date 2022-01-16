b48fce3bb7f61ac3818c5abf0fb018648e023a12
Bhavik27 <bhavik27071999@gmail.com>
2022-01-16 19:22:05 +0530

    34. Handling Errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,16 +1,24 @@
 const request = require('postman-request');
 
-const access_token = "  " // enter your access token
-const MAPBOX_API_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" + access_token;
+const access_token = "" // enter your access token
+const MAPBOX_API_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20ngeles.json?access_token=" + access_token;
 
 request(MAPBOX_API_URL, (error, response) => {
     if (error) {
         console.log('Unable to connect REST API');
     }
+    else if (response.body.error) {
+        console.log('Unable to find location.');
+    }
     else {
         const data = JSON.parse(response.body)
-        const logtitude = data.features[0].center[0]
-        const latitude = data.features[0].center[1]
-        console.log(logtitude + "," + latitude);
+        if (data.features.length == 0) {
+            console.log('No match found!');
+        }
+        else {
+            const logtitude = data.features[0].center[0]
+            const latitude = data.features[0].center[1]
+            console.log(logtitude + "," + latitude);
+        }
     }
 })
