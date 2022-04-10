f99f5cef2983e3294e9e5af4f4d5d68ff4626471
Bhavik27 <bhavik27071999@gmail.com>
2022-04-10 15:08:07 +0530

    48. Customizing the Views Directory

--- 7.web-server/src/app.js
+++ 7.web-server/src/app.js
@@ -1,13 +1,19 @@
 const path = require('path')
-const express = require('express');
 
+const express = require('express');
 const app = express()
 
 const PORT = 3000;
+
+// define path to express config
 const PUBLIC_DIRECTORY = path.join(__dirname, '../public/')
+const VIEW_TEMPLATES = path.join(__dirname, '../templates/')
 
-
+// setup handlers engine and view locations
 app.set('view engine', 'hbs')
+app.set('views', VIEW_TEMPLATES)
+
+// setup static directory to use
 app.use(express.static(PUBLIC_DIRECTORY))
 
 
@@ -24,6 +30,14 @@ app.get('/about', (req, res) => {
         createdBy: 'Bhavik'
     })
 })
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        createdBy: 'Bhavik'
+    })
+})
+
 
 
 app.get('/weather', (req, res) => {
