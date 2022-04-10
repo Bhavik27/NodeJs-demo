1837b6b81d241aff92d82397c1ade86ce9e3c572
Bhavik27 <bhavik27071999@gmail.com>
2022-04-10 14:57:53 +0530

    47. Dynamic Pages with Templating

--- 7.web-server/src/app.js
+++ 7.web-server/src/app.js
@@ -7,8 +7,24 @@ const PORT = 3000;
 const PUBLIC_DIRECTORY = path.join(__dirname, '../public/')
 
 
-
+app.set('view engine', 'hbs')
 app.use(express.static(PUBLIC_DIRECTORY))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Node App',
+        Heading: 'Static page - HBS'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Us',
+        createdBy: 'Bhavik'
+    })
+})
+
 
 app.get('/weather', (req, res) => {
     res.send({
