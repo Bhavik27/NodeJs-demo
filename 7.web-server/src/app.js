f562bddfb71715875a10e7ae571941a3632d4087
Bhavik27 <bhavik27071999@gmail.com>
2022-04-10 15:23:28 +0530

    49. Advanced Templating

--- 7.web-server/src/app.js
+++ 7.web-server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 const express = require('express');
+const hbs = require('hbs');
 const app = express()
 
 const PORT = 3000;
@@ -8,10 +9,12 @@ const PORT = 3000;
 // define path to express config
 const PUBLIC_DIRECTORY = path.join(__dirname, '../public/')
 const VIEW_TEMPLATES = path.join(__dirname, '../templates/')
+const PARTIALS = path.join(__dirname, '../templates/partials')
 
 // setup handlers engine and view locations
 app.set('view engine', 'hbs')
 app.set('views', VIEW_TEMPLATES)
+hbs.registerPartials(PARTIALS)
 
 // setup static directory to use
 app.use(express.static(PUBLIC_DIRECTORY))
@@ -19,21 +22,22 @@ app.use(express.static(PUBLIC_DIRECTORY))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Node App',
-        Heading: 'Static page - HBS'
+        title: 'APPLICATION',
+        createdBy: 'Bhavik'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About Us',
+        title: 'ABOUT US',
         createdBy: 'Bhavik'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Help',
+        title: 'HELP',
+        helpMessage:'You need some help?',
         createdBy: 'Bhavik'
     })
 })
